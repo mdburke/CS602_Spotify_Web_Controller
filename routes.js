b2ca3cd1df769d95b525c567fa0d381c47c16314
Michael Burke <mike.d.burke@gmail.com>
2018-02-05 15:40:11 -0500

    Start work on html markup and login as well as cookie handling

--- routes.js
+++ routes.js
@@ -4,7 +4,22 @@ const router = express.Router();
 // Router specs
 // Home
 router.get('/', (req, res, next) => {
-    res.render('home', { subtitle: 'Home' });
+    if (!req.session.name) {
+        res.redirect('login');
+    } else {
+        res.render('home', { subtitle: 'Now Playing', name: req.session.name });
+    }
 });
+
+router.get('/login', (req, res, next) => {
+    res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+    let name = req.body.name;
+    req.session.name = name;
+    res.redirect('/');
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
 const spotifyAuth = require('./spotify/spotifyAuth');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+// We should hold the secrets some place else, but for now this is ok...
+const secrets = require('./resources/secrets');
 
 const app = express();
 
@@ -17,11 +21,16 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Cookies
+app.use(cookieParser());
+app.use(expressSession(
+    {secret: secrets.dev.cookie_secret, resave: false, saveUninitialized: false}));
+
 // Routing
 const routes = require('./routes');
 app.use('/', routes);
 
-// Catch-all 404 error page
+// Catch-all 404.handlebars error page
 app.use((req, res) => {
     res.status(404);
     res.render('404');
